3c5445cda3a9729a8534aee18a8c71c61c87011e
Dhruv1217 <sharma.dhruv1217@gmail.com>
2026-02-08 01:25:15 +0530

    Add files via upload

---
+++ Assignment 1 (Task 1).py
@@ -0,0 +1,23 @@
+#TASK 1
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+#Addition
+
+print("Addition: ", num1+num2)
+
+#Subtraction
+
+print("Subtraction: ", num1-num2)
+
+#Multiplication
+
+print("Multiplication: ", num1*num2)
+
+#Division
+
+print("Division: ", num1/num2)
+
+           
+            
